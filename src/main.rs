066a5bbce7fa65b9bfca905e5b00a84a81a2deff
meipeter <meipeter114514@outlook.com>
2025-07-19 18:28:26 +0800

    modify cargo.toml

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use puddle_farm_api_client_openapi_client::apis::{
     configuration::Configuration,
     default_api::{player_id_get, top_get},
 };
+use serde_json::to_string_pretty;
 use tokio;
 #[tokio::main]
 async fn main() {
     println!(
-        "Top players:{:?}",
-        top_get(&Configuration::new()).await.unwrap()
+        "{}",
+        to_string_pretty(&top_get(&Configuration::new()).await.unwrap()).unwrap()
     );
 
     // println!(
